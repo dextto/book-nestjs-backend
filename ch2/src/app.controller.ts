17f5fc4131fd9df01cdd877060773ef238be2573
dexter <dexter.haan@gmail.com>
2021-09-23 16:35:06 +0900

    add: 프로퍼티 기반 주입 예시 추가

--- ch2/src/app.controller.ts
+++ ch2/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ServiceB } from './service-B';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly serviceB: ServiceB,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/serviceB')
+  getHelloC(): string {
+    return this.serviceB.getHello();
   }
 }
--- ch2/src/app.module.ts
+++ ch2/src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ApiController } from './api-controller/api-controller.controller';
+import { BaseService } from './base-service';
+import { ServiceA } from './service-A';
+import { ServiceB } from './service-B';
 
 @Module({
   controllers: [ApiController, AppController],
-  providers: [AppService],
+  providers: [AppService, BaseService, ServiceA, ServiceB],
   imports: [UsersModule],
 })
 export class AppModule {}
