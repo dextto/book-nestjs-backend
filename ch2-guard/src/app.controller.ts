d72614cea5ace90f9c3d562d64335b394b770a6e
dexter <dexter.haan@gmail.com>
2021-10-06 22:25:11 +0900

    add: guard

---
+++ ch2-guard/src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthGuard } from './auth.guard';
+
+// @UseGuards(AuthGuard)
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  // @UseGuards(AuthGuard)
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
---
+++ ch2-guard/src/auth.guard.ts
@@ -0,0 +1,16 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    return this.validateRequest(request);
+  }
+
+  private validateRequest(request: Request) {
+    return false;
+  }
+}
