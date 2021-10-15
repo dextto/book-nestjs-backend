d849cccbef97a9e23e0ffdc4e25f9d04d2eb0042
dexter <dexter.haan@gmail.com>
2021-10-15 20:42:08 +0900

    chore: rename folder

--- ch3-dynamic-module/src/app.module.ts
+++ ch3-dynamic-module/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import * as path from 'path';
 
 @Module({
   imports: [ConfigModule.forRoot({
