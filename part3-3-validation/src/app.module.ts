0da0f1eb030f89ba8e45616228417eb375f8f6cd
dexter <dexter.haan@gmail.com>
2021-10-28 23:00:52 +0900

    add: part3-3-validation

---
+++ part3-3-validation/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
