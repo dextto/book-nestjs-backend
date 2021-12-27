91334e011a6c9105487ef1dd07fe0fe8115a4d87
dexter <dexter.haan@gmail.com>
2021-12-27 11:48:33 +0900

    chore: rename

--- examples/ch12-exeption-filter/src/app.module.ts
+++ examples/ch12-exeption-filter/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './http-exception.filter';
 import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
   imports: [UsersModule],
 })
 export class AppModule { }
